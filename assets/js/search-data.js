7cf9ef0bdf2335dd8c63d3424ded366cae5b36cd
JiashenWangg <132632046+JiashenWangg@users.noreply.github.com>
2026-03-23 03:42:13 +0000

    Deploying to gh-pages from @ JiashenWangg/JiashenWangg.github.io@1aff0721e2b303fddae1d3d513230b4199024e9d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of my projects",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-repositories",
+  },{id: "nav-repositories",
           title: "repositories",
           description: "Following are my project repositories",
           section: "Navigation",
